e4da88113a31e38cd7c7339d4d89f6e22194de4c
Snehal Patkar <snehal.patkar@prudential.com>
2018-06-22 13:11:41 -0400

    after enabiling polyfillys it fails

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,10 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+/***
+IE9, IE10 and IE11 requires all of the following polyfills.
+**/
+
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es6/function';
@@ -37,43 +40,87 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
-/** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
-
-
-/** Evergreen browsers require these. **/
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
-
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
+/** IE10 and IE11 requires the following to support `@angular/animation`. */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
-/**
- * By default, zone.js will patch all possible macroTask and DomEvents
- * user can disable parts of macroTask/DomEvents patch by setting following flags
- */
+/*** Evergreen browsers require these. **/
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
 
- // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- // (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+/*** Babel Polyfill **/
+import '../node_modules/babel-polyfill/dist/polyfill.min.js';
 
- /*
- * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
- * with the following flag, it will bypass `zone.js` patch for IE/Edge
- */
-// (window as any).__Zone_enable_cross_context_check = true;
+/*** ALL Firefox browsers require the following to support `@angular/animation`. **/
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /***************************************************************************************************
- * Zone JS is required by default for Angular itself.
+ * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/**
+ * Date, currency, decimal and percent pipes.
+ * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
+ */
+// import 'intl';  // Run `npm install --save intl`.
+/**
+ * Need to import at least one locale-data with intl.
+ */
+// import 'intl/locale-data/jsonp/en';
+
+/* Declare polyfills as global */
+declare global {
+  interface String {
+    replaceMatches(json: JSON): string;
+  }
+}
+/*
+  This utility function matches json's attributes into a String
+  using ES6 string templates sintax ${attr} and replace them using json's values.
+  Notice that only strings are supported! It won't work with expressions or inner obejects.
+  Example: 'Hello, my name is ${name}!'.replaceMatches({name: 'Jhon'})
+  will return in 'Hello, my name is Jhon!'
+ */
+String.prototype.replaceMatches = function(json): any {
+  if (!json) {
+    // tslint:disable-next-line:no-invalid-this
+    return this;
+  }
+
+  const regx = new RegExp(
+    Object.keys(json)
+      .map(key => {
+        return `\\$\\{${key}\\}`; // places the key inside ${}
+      })
+      .join('|'),
+    'ig'
+  );
+
+  // tslint:disable-next-line:no-invalid-this
+  return this.replace(regx, match => {
+    const key = match.match(/\{(.*)\}/)[1]; // gets the key inside ${}
+
+    return json[key];
+  });
+};
+
+/* Object assign polyfills for IE */
+// tslint:disable-next-line:only-arrow-functions
+Object.assign =
+  Object.assign ||
+  function(t): any {
+    for (let s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (const p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) {
+          t[p] = s[p];
+        }
+      }
+    }
+
+    return t;
+  };
